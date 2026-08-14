99c0cf472992e9f772eb941737bee293607541e1
huyendieu8304 <huyendieu8304@gmail.com>
2026-08-14 10:36:04 +0700

    initial commit

---
+++ src/main/java/com/hirewise/be/HireWiseBeApplication.java
@@ -0,0 +1,13 @@
+package com.hirewise.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireWiseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HireWiseBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hirewise/be/HireWiseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirewise.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireWiseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
