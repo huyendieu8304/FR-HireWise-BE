28f222f195ac848629873cf65ce12bb5c5781976
huyendieu8304 <huyendieu8304@gmail.com>
2026-08-18 10:52:24 +0700

    authorization - ownership

---
+++ src/main/java/com/hirewise/be/domain/Role.java
@@ -0,0 +1,33 @@
+package com.hirewise.be.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 5 role mac dinh, is_system=true
+ */
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String code;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "is_system", nullable = false)
+    private boolean system;
+}
---
+++ src/main/java/com/hirewise/be/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.hirewise.be.repository;
+
+import com.hirewise.be.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByCode(String code);
+}
